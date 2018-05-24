8ad0da4dd985170d07abdc17ae128689dee91d81
Nadzir <nadzir.austin@gmail.com>
2018-05-25 01:30:02 +0800

    add babel

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import isEmpty from 'lodash/isEmpty'
+
 export const start = (swaggerFile, servername) => {
   if (isEmpty(swaggerFile)) console.error('Swagger File is empty')
   if (isEmpty(servername)) console.error('Server Name is empty')
 
-  console.log(start)
+  console.log('start')
 }
