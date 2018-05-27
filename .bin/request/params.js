0f2082d8cc69b95140a4395a79807680f12ae7bc
Nadzir <nadzir.austin@gmail.com>
2018-05-27 09:20:42 +0800

    modify import from main dir

--- .bin/request/params.js
+++ .bin/request/params.js
@@ -12,7 +12,6 @@ var _path2 = _interopRequireDefault(_path);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const getParams = exports.getParams = (wireSwagModel, wireSwagInput) => {
-  //   const model = require(path.join(process.env.PWD, `../../wireswag-input/controllers/${wireSwagModel}.js`))
   const model = require(_path2.default.join(process.env.PWD, `wireswag-input/controllers/${wireSwagModel}.js`));
   const values = model[wireSwagInput]();
 
--- src/request/params.js
+++ src/request/params.js
@@ -1,6 +1,5 @@
 import path from 'path'
 export const getParams = (wireSwagModel, wireSwagInput) => {
-//   const model = require(path.join(process.env.PWD, `../../wireswag-input/controllers/${wireSwagModel}.js`))
   const model = require(path.join(process.env.PWD, `wireswag-input/controllers/${wireSwagModel}.js`))
   const values = model[wireSwagInput]()
 
